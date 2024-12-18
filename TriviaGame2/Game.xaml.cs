8ed3193f0544f807b02cbc93a79d6a899c030c6e
obnwachukwue <g00435305@atu.ie>
2024-12-18 13:29:25 +0000

    created the button and the some setting in the settings page

--- TriviaGame2/Game.xaml.cs
+++ TriviaGame2/Game.xaml.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace TriviaGame2
 {
     internal class Game
     {
+
     }
 }
